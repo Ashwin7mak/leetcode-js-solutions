71593575f9fe19ccaa3ce8c17f8b376408ebc336
ashwin7mak <m.ashwinkumar7@gmail.com>
2019-02-04 00:29:36 -0800

    Valid Parentheses problem

---
+++ 20. Valid Parentheses/index.js
@@ -0,0 +1,35 @@
+var isValid = function(s) {
+    if (!s) return true;
+
+    let stack = [];
+
+    let left = ["(", "[", "{"];
+    let right = [")", "]", "}"];
+
+    let match = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (let i in s) {
+        // Check if the string has "(", then push it into stack
+        if (left.indexOf(s[i]) > -1) {
+            stack.push(s[i]);
+        }
+
+        // Check if the string has ")", then pop if from stack
+        if (right.indexOf(s[i]) > -1) {
+            let removed = stack.pop();
+
+            // Check for the match object for the corresponding characters if not return false
+            if (match[s[i]] != removed) {
+                return false;
+            }
+        }
+    }
+
+    // Check if the stack is empty or not
+    return stack.length == 0;
+
+};
