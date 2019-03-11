5567e8f6a14af5691ba3041bf45dcfda4f3ccdf0
ashwin7mak <m.ashwinkumar7@gmail.com>
2019-03-10 20:47:30 -0700

    Count Primes problem

---
+++ 204. Count Primes/index.js
@@ -0,0 +1,30 @@
+var countPrimes = function(n) {
+    // Sieve of Eratoshtenes Algorithm
+    // Two arrays one for carrying initial numbers and another for resulting prime numbers
+    let prime = [];
+    let res = [];
+    // Set all the values in the array to true starting from 2 i.e. first prime num
+    for(let i = 2; i < n; i++) { prime[i] = true; }
+
+    // Loop throught the array 
+    // Setting multiples of i to false
+    for(let i = 2; i * i < n; i++) {
+        for(let j = i * i; j < n; j += i) {
+            prime[j] = false;
+        }
+    }
+
+    // Loop through the array
+    // Push only the true values into the result array
+    for(let i = 0; i < n; i++) {
+        if(prime[i]) {
+            res.push(i);
+        }
+    }
+    console.log("RESULT :", res);
+    return res.length;
+}
+
+
+console.log(countPrimes(10));
+console.log(countPrimes(2));
