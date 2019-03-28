275cfaa1f14a81a2d4c69807da8b0fca8ca01be2
ashwin7mak <m.ashwinkumar7@gmail.com>
2019-03-28 00:39:28 -0700

    Updated the solution for Container with most water problem

--- 11. Container With Most Water/index.js
+++ 11. Container With Most Water/index.js
@@ -1,3 +1,4 @@
+// Brute Force Approach
 function maxArea(height) {
     // Assume initial water area is minimum
     let max = Number.NEGATIVE_INFINITY;
@@ -12,3 +13,26 @@ function maxArea(height) {
     }
     return max;
 }
+
+// Binary Search Approach
+var maxArea = function (height) {
+    let max = Number.NEGATIVE_INFINITY;
+    // Assigning two pointers
+    // Left as 0, Right as height size
+    let left = 0, right = height.length - 1;
+
+    while (left < right) {
+        // Assigning min as the min of the heights
+        let min = Math.min(height[left], height[right]);
+        // Calculate the area as max
+        max = Math.max(max, min * (right - left));
+
+        // Untill both left and right meets at one height
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return max;
+};
