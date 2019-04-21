dfdbcbe4a6fa2bb336c43bf5fa49d9a0265feea6
ashwin7mak <m.ashwinkumar7@gmail.com>
2019-04-20 22:12:48 -0700

    Sliding Window Problem

--- 239. Sliding Window Maximum/index.js
+++ 239. Sliding Window Maximum/index.js
@@ -1,20 +1,46 @@
-var maxSlidingWindow = function (nums, k) {
-    // Check if nums is empty
-    if (nums.length === 0) return [];
-    let max = 0, result = [];
-    // Loop through nums.length - k elements in array
-    // Assign max to first element
-    for (let i = 0; i <= nums.length - k; i++) {
-        max = nums[i];
-        // Loop through first k elements in array
-        // Find max for the first k elements
-        for (let j = 1; j < k; j++) {
-            if (max < nums[i + j]) {
-                max = nums[i + j];
-            }
-        }
-        // Push max into result
-        result.push(max);
+var minWindow = function (s, t) {
+    //
+    let result = s + t;
+    // Two pointers
+    let start = 0, end = 0;
+    // Object for carrying t elements
+    let hash = {}
+
+    for (let i = 0; i < t.length; i++) {
+        if (!hash[t[i]]) hash[t[i]] = 1;
+        else hash[t[i]]++;
     }
+
+    let count = Object.keys(hash).length;
+
+    // Check with two pointers
+    while (end < s.length) {
+        let charEnd = s[end];
+
+        if (hash[charEnd]) {
+            hash[charEnd]--;
+
+            if (hash[charEnd] === 0) count--;
+        }
+
+        end++;
+
+        while (count === 0) {
+            let charStart = s[start];
+
+            if (hash[charStart]) {
+                hash[charStart]++;
+
+                if (hash[charStart] > 0) count++;
+
+                let subStr = s.slice(start, end);
+
+                if (subStr.length < result.length) result = subStr;
+            }
+            start++;
+        }
+    }
+
+    if (result.length === s.length + t.length) return '';
     return result;
 };
