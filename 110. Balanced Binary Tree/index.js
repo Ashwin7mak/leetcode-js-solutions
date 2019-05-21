b9d5e6c8eb0f243f636d56b8e4c0f1d10ab3c8ad
ashwin7mak <m.ashwinkumar7@gmail.com>
2019-05-20 22:07:43 -0700

    Added comments to Balanced Binary Tree problem

--- 110. Balanced Binary Tree/index.js
+++ 110. Balanced Binary Tree/index.js
@@ -11,17 +11,21 @@
  */
 var isBalanced = function (root) {
 
+    // DepthTree function to find the Max depth of the Sub trees
     let depthTree = function (root) {
         if (root === null) return 0;
 
-        return Math.max(depthTree(root.left), depthTree(root.right)) + 1;
+        let left_subtree = depthTree(root.left);
+
+        let right_subtree = depthTree(root.right);
+        // Check for the actual balanced tree |left_subtree - right_subtree| < 1
+        if (left_subtree == -1 || right_subtree == -1 || Math.abs(left_subtree - right_subtree) > 1) {
+            return -1;
+        }
+
+        return Math.max(left_subtree, right_subtree) + 1;
     }
-
-    if (root === null) return true;
-
-    let left_subtree = depth(root.left);
-
-    let right_subtree = depth(root.left);
-
-    return Math.abs(left_subtree, right_subtree) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    console.log("YESSS", depthTree(root));
+    // Check if the depthTree function returns 1 value
+    return depthTree(root) !== -1;
 };
