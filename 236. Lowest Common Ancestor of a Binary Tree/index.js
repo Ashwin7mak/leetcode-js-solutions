fbd6d7ef6e367f5073c49911628c6f2d90963d45
ashwin7mak <m.ashwinkumar7@gmail.com>
2019-05-23 23:31:47 -0700

    LCA of a Binary Tree problem recursive solution

---
+++ 236. Lowest Common Ancestor of a Binary Tree/index.js
@@ -0,0 +1,21 @@
+var lowestCommonAncestor = function(root, p, q) {
+    // Check if root is null, return null
+    if(!root)
+        return null;
+    // Check if root is either of left or right values, return root
+    if(root == p || root == q)
+        return root;
+
+    // Perform recursive search on the left_subtree and right_subtree
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+
+    // If left is empty, return right and vice versa
+    if(!left) 
+        return right;
+    
+    if(!right)
+        return left;
+
+    return root;
+}
