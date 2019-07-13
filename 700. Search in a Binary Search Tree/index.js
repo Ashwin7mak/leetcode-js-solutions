dfac64e5fc4c9de145138d92319dce1cb142c8d6
ashwin7mak <m.ashwinkumar7@gmail.com>
2019-07-13 11:01:38 -0700

    Added search in a BST problem recursive solution

---
+++ 700. Search in a Binary Search Tree/index.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function (root, val) {
+
+    if (root == null) {
+        return [];
+    }
+    // Check if root is equal to val, return root
+    // Else if root is greater than val, return recursively left node
+    if (root.val == val) {
+        return root;
+    } else if (root.val > val) {
+        return searchBST(root.left, val);
+    } else {
+        return searchBST(root.right, val);
+    }
+
+};
