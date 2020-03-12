e3e234ab6d78cf97870eaac66f410c9004ce8c2a
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-03-11 23:31:45 -0700

    Added optimized solution

--- 1. TwoSum/index.js
+++ 1. TwoSum/index.js
@@ -1,6 +1,26 @@
 // Given nums = [2, 7, 11, 15], target = 9,
-//     Because nums[0] + nums[1] = 2 + 7 = 9,
+// Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1].
+
+// O(n)
+function TwoSum(nums, target) {
+    let result = new Map;
+
+    for (let i = 0; i < nums.length; i++) {
+        let result = new Map;
+
+        for (let i = 0; i < nums.length; i++) {
+            let a = target - nums[i];
+            if (result.has(a)) {
+                return [result.get(a), i];
+            }
+            result.set(nums[i], i);
+        }
+    }
+}
+
+
+// O(n^2)
 function TwoSum(nums, target) {
     let result = [];
 
