bc1b0324fa308af65e655f003624fdc9e0131b07
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-03-17 22:36:52 -0700

    Added design hashmap problem

---
+++ 706. Design HashMap/index.js
@@ -0,0 +1,63 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyHashMap = function () {
+    this.keys = [];
+    this.values = [];
+};
+
+/**
+ * value will always be non-negative. 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function (key, value) {
+    if (this.keys[key] === undefined) {
+        this.values.push(value);
+        this.keys[key] = this.values.length - 1;
+    } else {
+        this.values[this.keys[key]] = value;
+    }
+};
+
+/**
+ * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function (key) {
+    if (this.keys[key] === undefined) return -1;
+    return this.values[this.keys[key]];
+};
+
+/**
+ * Removes the mapping of the specified value key if this map contains a mapping for the key 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function (key) {
+    if (this.keys[key] !== undefined) {
+        this.values[this.keys[key]] = undefined;
+        this.keys[key] = undefined;
+
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
+
+let hashMap = new MyHashMap();
+hashMap.put(1, 1);
+hashMap.put(2, 2);
+hashMap.get(1);            // returns 1
+hashMap.get(3);            // returns -1 (not found)
+hashMap.put(2, 1);          // update the existing value
+hashMap.get(2);            // returns 1 
+hashMap.remove(2);          // remove the mapping for 2
+hashMap.get(2);            // returns -1 (not found) 
