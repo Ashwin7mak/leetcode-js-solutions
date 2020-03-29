43b8f3b4e89543ff13390557c8339b3a06c51390
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-03-28 21:21:43 -0700

    Added greedy approach solution to the maximum subarray problem

--- 53. Maximum SubArray/index.js
+++ 53. Maximum SubArray/index.js
@@ -12,3 +12,23 @@ var maxSubArray = function (nums) {
     }
     return maxSum;
 };
+
+/* Greedy Approach */
+var maxSubArray = function (nums) {
+    // Greedy Approach
+    if (nums.length == 0) return nums;
+
+    let maxSum = currSum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        // Compare two values and recalculate the currSum
+        currSum = Math.max(nums[i], currSum + nums[i]);
+        // Assign maxSum to currSum, if it is greater than maxSum
+        if (currSum > maxSum) {
+            maxSum = currSum;
+        }
+
+    }
+
+    return maxSum;
+};
