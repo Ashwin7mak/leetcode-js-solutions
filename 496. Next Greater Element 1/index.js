2ff7bd01d79840ddcf3af98d890734aebceb049f
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-04-07 23:55:48 -0700

    Updated solution for the Next Greater Element1 problem

--- 496. Next Greater Element 1/index.js
+++ 496. Next Greater Element 1/index.js
@@ -5,7 +5,6 @@
  */
 var nextGreaterElement = function (nums1, nums2) {
 
-
     // Use an object and empty stack to display the result
 
     // Iterate through the nums2 array
@@ -14,21 +13,23 @@ var nextGreaterElement = function (nums1, nums2) {
     // Push nums2[i] to stack
 
     // Now iterate through the nums1 array
-        // Ass
+        // Check if the nums[1] is found in the hash if so return the value else -1
 
     let hash = {};
 
     let stack = [];
 
     for (let i = 0; i < nums2.length; i++) {
-        console.log("---=", stack[stack.length - 1], nums2[i]);
+
         while (stack.length != 0 && stack[stack.length - 1] < nums2[i]) {
             let pop = stack.pop();
             hash[pop] = nums2[i];
         }
         stack.push(nums2[i]);
     }
-    console.log("S", stack);
-    console.log("====", hash);
+
     return nums1.map((e) => hash[e] || -1);
 };
+
+
+console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2]));
