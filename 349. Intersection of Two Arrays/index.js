98e9e153d05cee949cea0e3483077e1e46a3330b
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-04-20 23:39:54 -0700

    Updated solution for the intersection of the two arrays problem

--- 349. Intersection of Two Arrays/index.js
+++ 349. Intersection of Two Arrays/index.js
@@ -11,3 +11,32 @@ function intersection(nums1, nums2) {
     // Converting the array as Set thus removing the duplicates
     return [...new Set(res)];
 }
+
+/* Using Object(Hashmap) */
+var intersection = function (nums1, nums2) {
+    // Create new object and put one of the array entries in it
+    // Iterate through the other array entries check if there is a match found in the object
+        // If so push it to the result array
+        // Delete the found entry
+    let res = [];
+
+    let hash = {};
+
+    for (let i = 0; i < nums1.length; i++) {
+        let num = nums1[i];
+        if (!hash[num]) {
+            hash[num] = true;
+        }
+    }
+
+    for (let i = 0; i < nums2.length; i++) {
+        let num = nums2[i];
+        if (hash[num]) {
+            res.push(num);
+            delete hash[num];
+        }
+    }
+
+    return res;
+
+};
