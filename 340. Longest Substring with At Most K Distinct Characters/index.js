a4df51a64d810b1efdcf842e1f8d30cc3c3d4d4f
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-05-16 12:36:14 -0700

    Added Longest Substring with at most K distinct characters problem using Two pointers approach

---
+++ 340. Longest Substring with At Most K Distinct Characters/index.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var lengthOfLongestSubstringKDistinct = function (s, k) {
+    //if(s.length === 0 ||k === 0) { return 0; }
+
+    //if(s.length < k) return s.length === 1 ? 1 : s.length;
+    let left = 0, right = 0, hash = {};
+    let counter = 0;
+    let lengthOfSubstring = 0;
+
+    while (right < s.length) {
+        if (!hash[s[right]]) {
+            hash[s[right]] = 1;
+            counter++;
+        } else {
+            hash[s[right]]++;
+        }
+
+        while (counter === k + 1) {
+            hash[s[left]]--;
+            if (hash[s[left]] === 0) {
+                counter--;
+            }
+            left++;
+        }
+
+        right++;
+        lengthOfSubstring = Math.max(lengthOfSubstring, right - left);
+    }
+
+    return lengthOfSubstring;
+};
