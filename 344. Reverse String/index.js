1c5675227ff8730de57123268aacdb1c6bc6fbf5
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-06-04 21:55:01 -0700

    Added reverse string solution using two pointers approach

--- 344. Reverse String/index.js
+++ 344. Reverse String/index.js
@@ -1,3 +1,18 @@
 var reverseString = function (s) {
     return s.reverse();
 };
+
+/* Two Pointers Approach */
+
+var reverseString = function (s) {
+    if (s.length === 0 || s === null) return [];
+
+    let left = 0, right = s.length - 1;
+
+    while (left < right) {
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;
+        right--;
+    }
+    return s;
+};
