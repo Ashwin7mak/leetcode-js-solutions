d405b65a8dfab0e59f3e5e622c903b78e3da671c
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-06-08 23:48:50 -0700

    Added maximum depth of binary tree problem using iterative approach

--- 104. Maximum Depth of Binary Tree/index.js
+++ 104. Maximum Depth of Binary Tree/index.js
@@ -5,3 +5,33 @@ var maxDepth = function (root) {
     let right_depth = maxDepth(root.right);
     return Math.max(left_depth, right_depth) + 1;
 };
+
+
+/** Iterative Approach */
+var maxDepth = function (root) {
+
+    if (root == null) return 0;
+
+    let maxVal = -1;
+
+    let stack = [];
+
+    stack.push([root, 1]);
+
+    while (stack.length !== 0) {
+
+        let node = stack.pop();
+        let item = node[0];
+        let depth = node[1];
+
+        if (maxVal < depth) {
+            maxVal = depth;
+        }
+
+        if (item.left !== null) stack.push([item.left, depth + 1]);
+
+        if (item.right !== null) stack.push([item.right, depth + 1]);
+
+    }
+    return maxVal;
+};
