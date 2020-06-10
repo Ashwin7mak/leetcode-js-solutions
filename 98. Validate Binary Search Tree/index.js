f8234b5143b10b9b88f04096bea6f889a5a875b1
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-06-09 20:28:56 -0700

    Added validate binary search tree problem using iterative approach

--- 98. Validate Binary Search Tree/index.js
+++ 98. Validate Binary Search Tree/index.js
@@ -17,3 +17,29 @@ var isValidBST = function (root) {
     }
 
 };
+
+/* Iterative Approach */
+// Checking if the given tree can be presented in Inorder Traversal and check if the nodes are in increasing order or not
+
+function isValidBST(root) {
+    if(root === null) return true;
+
+    let result = [];
+
+    let minVal = -Infinity;
+
+    while(result.length > 0 || root !== null) {
+        if(root !== null) {
+            result.push(root);
+            root = root.left;
+        } else {
+            root = result.pop();
+            if(root.val <= minVal) {
+                return false;
+            }
+            minVal = root.val;
+            root = root.right;
+        }
+    }
+    return true;
+}
