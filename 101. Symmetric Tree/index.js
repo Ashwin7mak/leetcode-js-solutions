c2b085b2785977cd4f920fba4425b8ca0cb397b2
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-06-10 23:38:48 -0700

    Added symmetric tree problem using iterative approach

--- 101. Symmetric Tree/index.js
+++ 101. Symmetric Tree/index.js
@@ -22,3 +22,48 @@ var isSymmetric = function (root) {
 
 };
 
+/* Recursive Approach */
+function isSymmetric(root) {
+    if(root === null) return true;
+
+    return isMirror(root.left, root.right);
+
+    function isMirror(s, t) {
+        if(!s && !t) return true;
+
+        if(!s || !t || s.val !== t.val) return false;
+
+        return isMirror(s.left, t.right) && isMirror(s.right, t.left);
+    }
+} 
+
+/* Iterative Approach */
+
+function isSymmetric(root) {
+    if(root === null) return true;
+
+    return isMirror(root.left, root.right);
+
+    function isMirror(s, t) {
+        let arr1 = [s], arr2 = [t];
+
+        while(arr1.length > 0 || arr2.length > 0) {
+            let node1 = arr1.pop();
+            let node2 = arr2.pop();
+
+            if(!node1 && !node2) continue;
+
+            if (!node1 || !node2 || node1.val !== node2.val) {
+                return false;
+            }
+
+            arr1.push(node1.left);
+            arr1.push(node1.right);
+            arr2.push(node2.right);
+            arr2.push(node2.left);
+        }
+        return true;
+    }
+}
+
+
