ff1dc292c5b95d1dcf602be80fcbb81637f48790
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-06-13 17:48:37 -0700

    Added merge two sorted lists problem using iterative approach

--- 21. Merge Two Sorted Lists/index.js
+++ 21. Merge Two Sorted Lists/index.js
@@ -23,3 +23,40 @@ var mergeTwoLists = function (l1, l2) {
         return l2;
     }
 };
+
+/* Iterative Approach */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+    // A new node for reference as head
+    let temp = new ListNode(-1);
+    let head = temp;
+
+    // Compare two lists until they are compated
+    // Assign the smaller to the temp.next 
+    while (l1 !== null && l2 !== null) {
+        if (l1.val < l2.val) {
+            temp.next = l1;
+            l1 = l1.next;
+        } else {
+            temp.next = l2;
+            l2 = l2.next;
+        }
+        temp = temp.next;
+    }
+    // At this point, anyone of two lists will be empty
+    // Patch the non-empty list to the end of the merged list
+    temp.next = l1 === null ? l2 : l1;
+
+    return head.next;
+};
