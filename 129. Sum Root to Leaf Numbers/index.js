7dd197497277fb38f7438a1d7b5ad798b6a3dd18
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-06-26 20:55:35 -0700

    Added sum root to leaf numbers problem using iterative approach

--- 129. Sum Root to Leaf Numbers/index.js
+++ 129. Sum Root to Leaf Numbers/index.js
@@ -31,3 +31,47 @@ var sumNumbers = function (root) {
 
     return sumOfTree(root, 0);
 }
+
+/* Iterative Approach */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function (root) {
+    if (!root || root === null) return 0;
+    let totalSum = 0;
+    let queue = [];
+    // Pushing both root and value 
+    queue.push([root, 0]);
+
+    while (queue.length > 0) {
+        let node = queue.pop();
+
+        let item = node[0];
+        let value = node[1];
+
+        if (item !== null) {
+            // 1-> 2 = 12 ; 1->2->4 = 123
+            value = (value * 10 + item.val);
+
+            if (!item.left && !item.right) {
+                totalSum += value;
+            }
+
+            if (item.left) queue.push([item.left, value]);
+
+            if (item.right) queue.push([item.right, value]);
+        }
+    }
+
+    return totalSum;
+};
