f51db7bb1b345216a5ee9c90f20d82201d2693ed
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-07-05 21:15:59 -0700

    Added number of 1 Bits problem using bit manipulation approach

--- 191. Number of 1 Bits/index.js
+++ 191. Number of 1 Bits/index.js
@@ -6,3 +6,16 @@ var hammingWeight = function (n) {
     // Convert n to binary number,split at "0", join then count the length
     return n.toString(2).split("0").join("").length;;
 };
+
+/* Bit Manipulation approach */
+// 11 & 1 -> 1     10 & 1 -> 0
+// All odd numbers will have 1 bit
+// All even numbers will have 0 bit
+function hammingWeight = (n) => {
+    let count = 0;
+    while(n) {
+        count += 1;
+        n &= n-1;
+    }
+    return count;
+}
