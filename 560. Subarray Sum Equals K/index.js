ad9943e89459b14e37857d5b90a71f2db41a2aab
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-08-21 18:58:04 -0700

    Added subarray sum equals k using brute force approach for better space complexity

--- 560. Subarray Sum Equals K/index.js
+++ 560. Subarray Sum Equals K/index.js
@@ -3,15 +3,18 @@
  * @param {number} k
  * @return {number}
  */
-/* Hash map approach */
-var subarraySum = function (nums, k) {
-
+/* Hash map approach 
     // Use a hash map to store the nums 
     // Set the currSum as key and the no of occurences as values
     // Calculate the currSum at every iteration
         // Check if currSum - k is in the map
             // If so, reassign result += hash[currSum - k]
     // If currSum is not in the map, assign it to 1 else increment the value by 1
+    
+    TC - O(n)
+    SC - O(n)
+*/
+var subarraySum = function (nums, k) {
 
     let hash = {};
 
@@ -19,7 +22,7 @@ var subarraySum = function (nums, k) {
 
     let currSum = 0;
 
-    hash[0] = 1;   
+    hash[0] = 1;    // Default value
 
     for (let i = 0; i < nums.length; i++) {
         
@@ -36,4 +39,31 @@ var subarraySum = function (nums, k) {
         }
     }
     return result;
+};
+
+/* Brute Force Approach 
+
+    Iterate using two for loops
+        For each index i, find the continuous sum using j
+        check if currSum = k, if so increment res
+    
+    TC - O(n^2)
+    SC - O(1)
+*/
+
+var subarraySum = function (nums, k) {
+    let res = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        let currSum = 0;
+
+        for (let j = i; j < nums.length; j++) {
+
+            currSum += nums[j];
+            if (currSum === k) {
+                res += 1;
+            }
+        }
+    }
+    return res;
 };
