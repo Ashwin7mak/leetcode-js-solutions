4f7e3d3c4503830aeaa535008c4f5fbfcb2b6954
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-08-23 22:59:59 -0700

    Added path sum 3 problem using iterative approach

--- 437. Path Sum 3/index.js
+++ 437. Path Sum 3/index.js
@@ -1,5 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number}
+ */
+/* Iterative Approach 
+
+    - Idea, injecting node, PathSum, array of nodes in the path and hash
+    - Similar to subarray of sum k problem,
+    - Checking at the each iteration if there is a matching sum 
+*/
+var pathSum = function (root, sum) {
+
+    if (!root) return 0;
+
+    let result = 0;
+
+    let stack = [];
+    stack.push([root, root.val, [root.val], { '0': 1 }]);
+
+    while (stack.length > 0) {
+
+        let [node, pathSum, temp, hash] = stack.pop();
+
+        if (hash[pathSum - sum] !== undefined) {
+            result += hash[pathSum - sum];
+        }
 
 
+        let tempHash = Object.assign({}, hash);
+        if (tempHash[pathSum] === undefined) {
+            tempHash[pathSum] = 1;
+        } else {
+            tempHash[pathSum]++;
+        }
+
+
+        if (node.left) {
+            let currPath = [...temp];
+            currPath.push(node.left.val);
+
+            stack.push([node.left, pathSum + node.left.val, currPath, tempHash]);
+        }
+
+        if (node.right) {
+            let currPath = [...temp];
+            currPath.push(node.right.val);
+
+            stack.push([node.right, pathSum + node.right.val, currPath, tempHash]);
+        }
+    }
+    return result;
+};
+
+/* Recursive Approach */
 const pathSum = function(root, sum) {
 
     let count = 0;
