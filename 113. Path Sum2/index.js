3b524385dd05e64c4e2e06ed57bfbdeacfa5c4f1
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-08-23 23:03:58 -0700

    Added path sum 2 problem using iterative approach

--- 113. Path Sum2/index.js
+++ 113. Path Sum2/index.js
@@ -1,8 +1,9 @@
 /**
  * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
  * }
  */
 /**
@@ -10,6 +11,44 @@
  * @param {number} sum
  * @return {number[][]}
  */
+/* Iterative Approach 
+    - Injecting node, node.val and array of elements in the path
+    - Check if there is a path from root to leaves, if so push the array in the result
+*/
+var pathSum = function (root, sum) {
+
+    if (!root) return [];
+
+    let result = [], stack = [];
+
+    stack.push([root, root.val, [root.val]]);
+
+    while (stack.length > 0) {
+
+        let [node, pathSum, temp] = stack.pop();
+
+        if (!node.left && !node.right) {
+            if (pathSum === sum) {
+                result.push(temp);
+            }
+        }
+
+        if (node.left) {
+            let currPath = [...temp];
+            currPath.push(node.left.val);
+            stack.push([node.left, pathSum + node.left.val, currPath]);
+        }
+
+        if (node.right) {
+            let currPath = [...temp];
+            currPath.push(node.right.val);
+            stack.push([node.right, pathSum + node.right.val, currPath]);
+        }
+    }
+    return result;
+};
+
+/* Recursive Approach */
 var pathSum = function (root, sum) {
     return findPaths(root, sum, [], []);
 };
