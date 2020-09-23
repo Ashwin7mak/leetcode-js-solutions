2f59a195cc56fb3b893332b39fa11c7f53722578
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-09-22 20:08:27 -0700

    Updated solution for the plus one problem

--- 66. Plus One/index.js
+++ 66. Plus One/index.js
@@ -1,3 +1,34 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+/*
+    - Iterate from the last digit of the digits
+        - If digit is 9, set the digit to 0
+        - else add 1 to the digit and return the digit
+    
+    - If the left most digit is 9, we need to add 1 to it
+    - Create an array with 1, add digit to it.
+    - return the resulting array
+*/
+var plusOne = function (digits) {
+
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] === 9) {
+            digits[i] = 0;
+        } else {
+            digits[i] += 1;
+            return digits;
+        }
+    }
+
+    let result = [1];   // Only if the left most digit is 9, we need to add 1 infront of it
+    result.push(...digits);
+
+    return result;
+};
+
+/* Another Approach */
 var plusOne = function (digits) {
     // Loop through the digits
     let i = digits.length - 1;
