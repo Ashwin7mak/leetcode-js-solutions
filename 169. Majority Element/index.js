8ea4fbbe04e343e02f9cb4976dd2904d3ccd745b
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-09-22 22:20:02 -0700

    Added majority element problem using Bayer-Moore's approach

--- 169. Majority Element/index.js
+++ 169. Majority Element/index.js
@@ -1,3 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+/*
+    Bayer-Moore's Algorithm Approach
+    TC - O(n)
+    SC - O(1)
+*/
+var majorityElement = function (nums) {
+
+    let candidate = 0, count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (count === 0) {
+            candidate = nums[i];
+        }
+
+        if (nums[i] !== candidate) {
+            count--;
+        } else {
+            count++;
+        }
+    }
+    return candidate;
+};
+
+/*
+    HashTable Approach
+    TC - O(n)
+    SC - O(n)
+*/
 var majorityElement = function (nums) {
     let hash = {};
 
