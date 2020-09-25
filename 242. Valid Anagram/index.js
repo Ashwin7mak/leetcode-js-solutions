14d82a355f0235303d2056331cf953aa20cf35a1
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-09-24 20:13:13 -0700

    Updated valid anagram solution using array masking approach

--- 242. Valid Anagram/index.js
+++ 242. Valid Anagram/index.js
@@ -1,3 +1,43 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+/*
+    Array Masking Approach
+    - Create two arrays of size 26 and fill with 0s
+    - As we iterate through the string, we fill the corresponding indices (ASCII values of each character) with incremented value. Eg: 'a' - 0, 'b' - 1, ... etc
+    - Finally, compare the characters in two arrays, if they differ return false else true
+    TC O(n)
+    SC O(1)
+*/
+var isAnagram = function (s, t) {
+    
+    if (s.length !== t.length) return false;
+    let arr1 = new Array(26).fill(0);
+    let arr2 = new Array(26).fill(0);
+
+    for (let ch of s) {
+        arr1[ch.charCodeAt() - 'a'.charCodeAt()]++;
+    }
+
+    for (let ch of t) {
+        arr2[ch.charCodeAt() - 'a'.charCodeAt()]++;
+    }
+
+    for (let i = 0; i < 26; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+/* 
+    Sort Approach 
+    TC O(nlogn)
+    SC O(1)
+*/
 var isAnagram = function (s, t) {
     // Check the sorted strings are equal
     return s.split("").sort().join("") === t.split("").sort().join("");
