d3a3e5a13db50f5ed88c9ce5b2a539e52741cf99
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-09-25 22:50:25 -0700

    Added largest number problem using custom sorting approach

---
+++ 179. Largest Number/index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+/*
+    - Convert given array of integers to array of strings
+    - Perform sorting my concating each character
+
+    TC - O(n logn)
+    SC - O(1)
+*/
+var largestNumber = function (nums) {
+
+    if (!nums || nums.length === 0) return nums.toString();
+
+    let result = '';
+
+    let largestNumber = nums.map(String).sort((a, b) => {
+        if (a.concat(b) > b.concat(a)) {
+            return -1;
+        }
+        return 1;
+    });
+
+    if (largestNumber[0] === "0") return "0";
+
+    return largestNumber.join('');
+};
