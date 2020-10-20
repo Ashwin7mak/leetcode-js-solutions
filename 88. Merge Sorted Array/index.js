47467794563a54ef6c5836afc2007164276d750d
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-10-20 11:03:09 -0700

    Added merge sorted array problem using two pointers approach

--- 88. Merge Sorted Array/index.js
+++ 88. Merge Sorted Array/index.js
@@ -1,3 +1,39 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+/*
+    Two Pointers Inplace Modification Approach
+    - while(n > 0),
+    - Iterate from the last of the initialized elements in both the arrays simultaneously, 
+        assign the greater one to the end of the nums1 array
+    
+    TC - O(n)
+    SC - O(1)
+
+*/
+var merge = function(nums1, m, nums2, n) {
+    while(n > 0) {
+        if(m >= 0 && nums1[m - 1] > nums2[n - 1]) {
+            nums1[m + n - 1] = nums1[m - 1];
+            m--;
+        } else {
+            nums1[m + n - 1] = nums2[n - 1];
+            n--;
+        }
+    }
+    return nums1;
+};
+
+
+/*
+    TC - O(n log n);
+    SC - O(1)
+*/
+
 var merge = function (nums1, m, nums2, n) {
     
     if (!n) return;
