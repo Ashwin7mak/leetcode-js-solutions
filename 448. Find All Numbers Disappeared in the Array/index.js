6427c9e4d3b2be727ebdf1ee3ed4b5eb3249e799
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-10-19 22:02:52 -0700

    Added find all numbers disappeared in the array problem using space inplace modification approach

--- 448. Find All Numbers Disappeared in the Array/index.js
+++ 448. Find All Numbers Disappeared in the Array/index.js
@@ -1,4 +1,42 @@
-function findDisappearedNumbers(nums) {
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+/*
+    Space Inplace Modification Approach
+    - Iterate through the array
+        - Use the Math difference of the current number - 1 as the index
+        - If the nums[index] > 0, store the negative value of nums[index] i.e. -nums[index]
+    - In the second iteration from 1 to n including n
+        - Check if any value that is positive, push those i values into result array
+    - Return result
+
+    TC - O(n)
+    SC - O(1)
+*/
+var findDisappearedNumbers = function(nums) {
+    let result = [];
+    
+    for(let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i]) - 1;
+        if(nums[index] > 0)
+            nums[index] = -nums[index];
+    }
+    
+    for(let i = 1; i <= nums.length; i++) {
+        if(nums[i -  1] > 0) {
+            result.push(i);
+        }
+    }
+    
+    return result;
+};
+
+
+/*
+    Missing Index Approach
+*/
+var findDisappearedNumbers = function(nums) {
     let result = [];
 
     //Iterate through the loop
