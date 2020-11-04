0f6dc03da83dbb67efa08e7bcf43d53471f20373
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-11-04 08:26:40 -0800

    Updated maximum subarray using brute force approach

--- 53. Maximum SubArray/index.js
+++ 53. Maximum SubArray/index.js
@@ -13,9 +13,15 @@ var maxSubArray = function (nums) {
     return maxSum;
 };
 
-/* Greedy Approach */
+/* 
+    Greedy Approach (Kadane's Algorith)
+    - Calculate the maxSum at every number while iterating the array
+
+    TC - O(n)
+    SC - O(1)
+*/
 var maxSubArray = function (nums) {
-    // Greedy Approach
+
     if (nums.length == 0) return nums;
 
     let maxSum = currSum = nums[0];
@@ -23,12 +29,34 @@ var maxSubArray = function (nums) {
     for (let i = 1; i < nums.length; i++) {
         // Compare two values and recalculate the currSum
         currSum = Math.max(nums[i], currSum + nums[i]);
-        // Assign maxSum to currSum, if it is greater than maxSum
+        // Update maxSum if it is lesser than currSum
         if (currSum > maxSum) {
             maxSum = currSum;
         }
 
     }
 
+    return maxSum;
+};
+
+/*
+    Brute Force's Approach
+
+    TC - O(n ^ 2)
+    SC - O(1)
+*/
+var maxSubArray = function(nums) {
+    
+    let maxSum = -Infinity;
+    
+    for(let i = 0; i < nums.length; i++) {
+        let currSum = 0;
+        
+        for(let j = i; j < nums.length; j++) {
+            currSum += nums[j];
+            
+            maxSum = Math.max(currSum, maxSum);
+        }
+    }
     return maxSum;
 };
