1ef656ff44eaa9fb2c2abe067e6d0e2668108013
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-11-11 19:57:36 -0800

    Updated solution for the word pattern problem

--- 290. Word Pattern/index.js
+++ 290. Word Pattern/index.js
@@ -3,34 +3,52 @@
  * @param {string} str
  * @return {boolean}
  */
-var wordPattern = function (pattern, str) {
-    str = str.split(' ');
+/*
+    Hashmap Approach
+    - Split the string based upon the spaces
+        - Check if the str.length === pattern.length
+    -  Using object to store the string characters
+       Key - string pattern characters
+       Value - string str characters
+    - Iterate through the str
+        - ch = pattern[i], w = str[i]
+        - Check if ch is found in the hash
+            - If not check if the hash has w as its values, if so return false
+            - Add hash[ch] = w
+        - Since, ch is found in the hash
+            - Check if hash[ch] === w, if not return false
+    - return true
 
-    if (pattern.length !== str.length) return false;
-    // Using object to store the string characters
-    // Key - string pattern characters
-    // Value - string str characters
+    TC - O(n)
+    SC - O(n)
+*/
+var wordPattern = function (pattern, s) {
+    
+    if(!s && !p)
+        return true;
+    
+    if(!s || !pattern)
+        return false;
+    
+    let str = s.split(' ');
+    
+    if(pattern.length !== str.length)
+        return false;
+    
     let hash = {};
-    // Check if the key is present in hash
-        // If so, check if the key has the value assigned corresponding to string str
-            // If not return false
-    // If the key is not present in the hash
-        // Check if the hash has any value associated with the string str
-            // If so, return false
-            // If not, assign hash[key] to the string str
-    for (var i = 0; i < str.length; i++) {
-        if (hash[pattern[i]]) {
-            if (hash[pattern[i]] !== str[i]) {
+    
+    for(let i = 0; i < str.length; i++) {
+        
+        let ch = pattern[i], word = str[i];
+        
+        if(!hash.hasOwnProperty(ch)) {
+            if(Object.values(hash).indexOf(word) !== -1)
                 return false;
-            }
+            hash[ch] = word;
         } else {
-            if (Object.values(hash).indexOf(str[i]) !== -1) {
+            if(hash[ch] !== word)
                 return false;
-            } else {
-                hash[pattern[i]] = str[i];
-            }
-        }
+        }   
     }
-
-    return true;
+    return true;  
 };
