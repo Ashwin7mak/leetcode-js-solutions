07078b61b891189078338b54d2bd3dbdde0c9fba
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-11-13 17:09:32 -0800

    Updated minimum depth of binary tree problem using DFS iterative approach

--- 111. Minimum Depth of Binary Tree/index.js
+++ 111. Minimum Depth of Binary Tree/index.js
@@ -1,3 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+/* 
+    DFS Iteartive Approach
+    - Check the depth at the leaf nodes
+        - Return min depth 
+    
+    TC - O(n)
+    SC - O(n)
+*/
+
+var minDepth = function(root) {
+    
+    if(root === null) return 0;
+    
+    let stack = [[root, 1]];
+    
+    let minDepth = Infinity;
+    
+    while(stack.length > 0) {
+        
+        let [node, depth] = [...stack.pop()];
+        
+        if(node.left === null && node.right === null) {
+            if(minDepth > depth)
+                minDepth = depth;
+        }
+    
+        if(node.left) stack.push([node.left, depth + 1]);
+        
+        if(node.right) stack.push([node.right, depth + 1]);
+    }
+    return minDepth;
+};
+
+/*
+    Recursive Approach
+*/
 var minDepth = function (root) {
     if (root === null) return 0;
 
