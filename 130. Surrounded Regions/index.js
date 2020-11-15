dd33ea3d4c0608981b116b557e37aeef790e7043
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-11-14 20:08:26 -0800

    Updated surrounded regions solution using DFS approach

--- 130. Surrounded Regions/index.js
+++ 130. Surrounded Regions/index.js
@@ -2,6 +2,61 @@
  * @param {character[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
  */
+/*  
+    DFS Approach
+    - Iterate through the grid
+        - Check all the Os in the border,
+            - Perform DFS on it, convert all Os to *
+    
+    - Iterate through the grid
+        - If Os are encountered, change all Os to X
+        - If *s are encountered, change all *s to O
+
+    TC - O(n)
+    SC - O(n)
+*/
+var solve = function(board) {
+    
+    if(!board || board.length === 0) return board;
+    
+    let m = board.length, n = board[0].length;
+    
+    for(let i = 0; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            if(board[i][j] === 'O' && (i === 0 || i === m - 1 || j === 0 || j === n - 1))
+                dfs(board, i, j);
+        }
+    }
+    
+    for(let i = 0; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            if(board[i][j] === 'O')
+                board[i][j] = 'X';
+            
+            if(board[i][j] === '*')
+                board[i][j] = 'O';
+        }
+    }
+    
+    // DFS Function
+    function dfs(board, i, j) {
+        if(i < 0 || i >= m || j < 0 || j >= n || board[i][j] !== 'O')
+            return;
+        
+        board[i][j] = '*';
+        
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+    
+    return board;
+};
+
+/*
+    Another DFS Approach
+*/
 var solve = function (board) {
     // Check board length is not null
     if (board.length == 0 || board.length == null) {
